edd5b0b9b103cee317bdb7093fba0a8361a1db95
Subhankar Pati <82767744+Subhankar27@users.noreply.github.com>
2022-01-15 16:53:24 +0530

    Update network_scanner.py

--- network_scanner.py
+++ network_scanner.py
@@ -16,6 +16,8 @@ def scan(ip):
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose = False)[0]
+    
+    
 
     # print("IP\t\t\tMAC Adress\n-----------------------------------")
     clients_list = []
